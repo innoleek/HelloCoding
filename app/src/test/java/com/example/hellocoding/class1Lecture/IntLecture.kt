56be29d174ef3e879ea8a2c33796901a95b38537
Min <innoleek@gmail.com>
2021-05-05 18:57:27 -0700

    ml

--- app/src/test/java/com/example/hellocoding/class1Lecture/IntLecture.kt
+++ app/src/test/java/com/example/hellocoding/class1Lecture/IntLecture.kt
@@ -1,12 +1,5 @@
 package com.example.hellocoding.class1Lecture
 
-import org.junit.Test
-
 class IntLecture {
 
-    @Test
-    fun `Int`() {
-        var age: Int = 31
-        println(age)
-    }
 }
